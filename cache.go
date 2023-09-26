1f6daa3613d59bcdc9446c29d2ca6fc964c47253
SmartForger <23446619+SmartForger@users.noreply.github.com>
2023-09-26 01:49:15 -0700

    Add cache

---
+++ cache.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+var store = sync.Map{}
+
+func CSet(key string, value string) {
+	store.Store(key, value)
+}
+
+func CGet(key string) string {
+	data, found := store.Load(key)
+
+	if found {
+		return fmt.Sprintf("%v", data)
+	}
+
+	return ""
+}
